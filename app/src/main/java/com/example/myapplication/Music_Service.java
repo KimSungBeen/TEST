03c57afd240c68580cfff910cd11c59553b84199
SungBeen Kim <s_been@kakao.com>
2020-03-27 10:29:31 +0900

    BranchTest

--- app/src/main/java/com/example/myapplication/Music_Service.java
+++ app/src/main/java/com/example/myapplication/Music_Service.java
@@ -82,6 +82,16 @@ public class Music_Service extends Service {
             }
         });
 
+            musicHandler.post(new Thread() {
+                @Override
+                public void run() {
+                    while(true){
+
+                    }
+
+                }
+            });
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -96,6 +106,47 @@ public class Music_Service extends Service {
         }
 
     }
+
+//    //SeekBar 움직임 출력하는 쓰레드
+//    public static final Runnable moveSeekBarThread = new Runnable() {
+//        public void run() {
+//            try {
+//                if (mediaPlayer[musicNum].isPlaying()) {
+//                    int position = mediaPlayer[musicNum].getCurrentPosition(); //현재 재생위치
+//                    int musicTime = mediaPlayer[musicNum].getDuration(); //한 음악파일의 재생시간
+//                    musicSeekBar.setMax(musicTime);
+//                    musicSeekBar.setProgress(position);
+//
+//                    //음악의 현재시간, 총 재생시간 계산
+//                    int currentMinute = (position/1000)/60;
+//                    int currentSeconds = (position/1000)%60;
+//                    int allMinute = (musicTime/1000)/60;
+//                    int allSeconds = (musicTime/1000)%60;
+//
+//                    //음악의 현재시간, 총 재생시간 출력
+//                    if(currentSeconds < 10) {
+//                        String currentMusicTime = currentMinute + ":0" + currentSeconds;
+//                        TV_currentMusicTime.setText(currentMusicTime);
+//                    }else {
+//                        String currentMusicTime = currentMinute + ":" + currentSeconds;
+//                        TV_currentMusicTime.setText(currentMusicTime);
+//                    }
+//                    if(allSeconds < 10) {
+//                        String allMusicTime = allMinute + ":0" + allSeconds;
+//                        TV_allMusicTime.setText(allMusicTime);
+//                    }else {
+//                        String allMusicTime = allMinute + ":" + allSeconds;
+//                        TV_allMusicTime.setText(allMusicTime);
+//                    }
+//
+//                    //(SEEKBAR_INITIAL_TIME: 1000) 1초 마다 쓰레드동작
+//                    musicHandler.postDelayed(this, SEEKBAR_INITIAL_TIME);
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    };
 
     //SeekBar 움직임 출력하는 쓰레드
     public static final Runnable moveSeekBarThread = new Runnable() {
